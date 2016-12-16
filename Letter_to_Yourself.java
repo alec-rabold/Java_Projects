d6df7dbbc07b1390a05cfb4ebe72e4155e9ff8c3
Alec Rabold <alecrabold@outlook.com>
2016-12-15 21:38:00 -0800

    Basics and Printing

---
+++ Letter_to_Yourself.java
@@ -0,0 +1,17 @@
+public class Letter_to_Yourself
+{
+	public static void main(String[] args)
+	{
+		System.out.println("+----------------------------------------------------------+");
+		System.out.println("|                                                     #### |");
+		System.out.println("|                                                     #### |");
+		System.out.println("|                                                     #### |");
+		System.out.println("|                                                          |");
+		System.out.println("|                                                          |");	
+		System.out.println("|                                Alec Rabold               |");
+		System.out.println("|                                6124 Montezuma Rd         |");
+		System.out.println("|                                San Diego, CA 92115       |");
+		System.out.println("|                                                          |");	
+		System.out.println("+----------------------------------------------------------+");
+	}
+}
---
+++ My_Initials.java
@@ -0,0 +1,14 @@
+public class My_Initials
+{
+	public static void main(String[] args)
+	{
+		// This will print my initials row by row
+		System.out.println("  A      TTTTT    RRRR ");
+		System.out.println(" A A       T      R   R");
+		System.out.println("A   A      T      R   R");
+		System.out.println("AAAAA      T      RRRRR");
+		System.out.println("A   A      T      R R  ");
+		System.out.println("A   A      T      R  R ");
+		System.out.println("A   A      T      R   R");
+	}
+}
